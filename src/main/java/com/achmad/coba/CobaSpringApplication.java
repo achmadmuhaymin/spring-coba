4ae51247cdaa16e89f6b20892ced5a6f18dfedc0
achmadmuhaymin <achmadmuhaymin@gmail.com>
2018-07-31 13:43:47 +0700

    0.0

---
+++ src/main/java/com/achmad/coba/CobaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.achmad.coba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaSpringApplication.class, args);
+	}
+}
